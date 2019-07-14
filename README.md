54b61cff93a483b42ba843e85c2c70eba56b3f22
Sagleft <sagleft@inbox.ru>
2019-07-14 04:41:41 +0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+
 # TRIDcode
 Barcode для крипто-хешей
+
+![enter image description here](http://sagleft.ru/projects/TRID/trid_code.png)
--- render_code.php
+++ render_code.php
@@ -2,11 +2,12 @@
 	require "./src/TRIDcode.php";
 	
 	//пример без фильтрации данных
-	
+	$form = $_GET['form'];
 	$hash = "1ac1d825af09775bccda0971efdc37c3945b7c3671a2e558bbb10d18cdbe177b";
 	try {
-		$code = new TRIDcode($hash);
-		$code->render();
+		$code = new TRIDcode($hash, $form);
+		$code->render(20);
+		//exit(json_encode($code->code_arr));
 	} catch(Exception $ex) {
 		exit("error: " . $ex->getMessage());
 	}
